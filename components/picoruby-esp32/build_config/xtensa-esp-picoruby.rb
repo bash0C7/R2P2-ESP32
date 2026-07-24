56d1d1b54d9905b55762c0a8dfc9588b9999fa94
bash0C7 <ksb.4038.nullpointer+github@gmail.com>
2026-07-24 11:03:42 +0900

    fix(picoruby-esp32): drop stale picoruby-filesystem-fat refs, keep ESP32_PLATFORM alongside PICORB_PLATFORM_ESP32, point submodule at rebased+ble-bridge picoruby
    Trial commit for the upstream/master rebase verification, not yet landed on stackchan-integration.

--- components/picoruby-esp32/build_config/xtensa-esp-picoruby.rb
+++ components/picoruby-esp32/build_config/xtensa-esp-picoruby.rb
@@ -21,8 +21,8 @@ MRuby::CrossBuild.new('esp32-picoruby') do |conf|
   conf.cc.defines << 'MRB_32BIT'
   conf.cc.defines << 'PICORB_ALLOC_ESTALLOC'
   conf.cc.defines << 'PICORB_ALLOC_ALIGN=8'
-  conf.cc.defines << 'USE_FAT_FLASH_DISK'
   conf.cc.defines << 'NDEBUG'
+  conf.cc.defines << 'ESP32_PLATFORM'
   conf.cc.defines << "PICORB_PLATFORM_ESP32"
   conf.cc.defines << "USE_WIFI" if ENV['USE_WIFI']
 
