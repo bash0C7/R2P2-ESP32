2c75511968be82f8cf5808279b95a89381ce90d0
Yuhei Okazaki <okazaki@fusic.co.jp>
2026-09-04 10:12:29 +0900

    Add picoruby-irq

--- components/picoruby-esp32/build_config/riscv-esp-femtoruby.rb
+++ components/picoruby-esp32/build_config/riscv-esp-femtoruby.rb
@@ -39,6 +39,7 @@ MRuby::CrossBuild.new("esp32-femtoruby") do |conf|
   conf.gem core: 'picoruby-adc'
   conf.gem core: 'picoruby-uart'
   conf.gem core: 'picoruby-pwm'
+  conf.gem core: 'picoruby-irq'
 
   # others
   conf.gem core: 'picoruby-esp32'
